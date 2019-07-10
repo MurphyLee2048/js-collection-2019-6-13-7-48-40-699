c27a1bf21eb94d265b703ca51c5cd756b57e2132
murphylee2048 <1134697131@qq.com>
2019-07-10 15:15:46 +0800

    commit grouping_count.js(use filter with a violent way)

--- practices/filter/grouping_count.js
+++ practices/filter/grouping_count.js
@@ -1,8 +1,31 @@
 'use strict';
 
-function grouping_count(collection) {
+// function grouping_count(collection) {
+//
+//   //implement here
+//   var map = new Map();
+//   var count = 0;
+//   for (let i = 0; i < collection.length; i++) {
+//     if (map.has(collection[i])) {
+//       count = map.get(collection[i]);
+//       map.set(collection[i], ++count);
+//     }
+//   }
+// }
 
-  //implement here
-}
+const grouping_count = (collection) => {
+  const condition1 = (number) => {return number === 1;};
+  const condition2 = (number) => {return number === 2;};
+  const condition3 = (number) => {return number === 3;};
+  const condition4 = (number) => {return number === 4;};
+
+  let l1 = collection.filter(condition1).length;
+  let l2 = collection.filter(condition2).length;
+  let l3 = collection.filter(condition3).length;
+  let l4 = collection.filter(condition4).length;
+
+  return {'1':l1, '2':l2, '3':l3, '4':l4};
+
+};
 
 module.exports = grouping_count;
